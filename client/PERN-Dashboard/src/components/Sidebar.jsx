4ea71377c7f3653a1fbcf877801e786354b2f7f0
Giovanni Cornejo <cornejog553@gmail.com>
2025-11-29 21:59:57 -0800

    created cleaners page

--- client/PERN-Dashboard/src/components/Sidebar.jsx
+++ client/PERN-Dashboard/src/components/Sidebar.jsx
@@ -21,62 +21,64 @@ export default function Sidebar() {
           </div>
           <div>Cleaning Crafters</div>
         </div>
-        
-          <NavLink
-            to="/"
-            end
-            className={({ isActive }) =>
-              isActive ? "text-blue-600 font-bold hover:text-white" : "hover:text-white"
-            }
-          >
-            <div className="flex flex-row justify-start ml-5 hover:bg-blue-600 hover:text-white">
-          <div className="flex flex-row gap-3 p-2">
-            <img src={dashboardLogo} alt="Dashboard icon" 
-            className="group-hover:brightness-0 group-hover:invert
-            "/>
-            Dashboard
+
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            isActive
+              ? "text-blue-600 font-bold hover:text-white"
+              : "hover:text-white"
+          }
+        >
+          <div className="flex flex-row justify-start ml-5 hover:bg-blue-600 hover:text-white">
+            <div className="flex flex-row gap-3 p-2">
+              <img
+                src={dashboardLogo}
+                alt="Dashboard icon"
+                className="group-hover:brightness-0 group-hover:invert
+            "
+              />
+              Dashboard
             </div>
-        </div>
-          </NavLink>
-          
-        
-          <NavLink
-            to="/bookings"
-            className={({ isActive }) =>
-              isActive ? "text-blue-600 font-bold" : "hover:text-blue-600"
-            }
-          >
-            <div className="flex flex-row items-center gap-3 p-2 ml-5 hover:bg-blue-600 hover:text-white">
+          </div>
+        </NavLink>
+
+        <NavLink
+          to="/bookings"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600 font-bold" : "hover:text-blue-600"
+          }
+        >
+          <div className="flex flex-row items-center gap-3 p-2 ml-5 hover:bg-blue-600 hover:text-white">
             <img src={bookingIcon} alt="Dashboard icon" />
             Bookings
-            </div>
-          </NavLink>
-        
-          <NavLink
-            to="/customers"
-            className={({ isActive }) =>
-              isActive ? "text-blue-600 font-bold" : "hover:text-blue-600"
-            }
-          >
-            <div className="flex flex-row items-center gap-3 p-2 ml-5 hover:bg-blue-600 hover:text-white">
-          <img src={customerIcon} alt="Dashboard icon" />
+          </div>
+        </NavLink>
+
+        <NavLink
+          to="/customers"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600 font-bold" : "hover:text-blue-600"
+          }
+        >
+          <div className="flex flex-row items-center gap-3 p-2 ml-5 hover:bg-blue-600 hover:text-white">
+            <img src={customerIcon} alt="Dashboard icon" />
             Customers
-            </div>
-          </NavLink>
-        
-        
-          <NavLink
-            to="/cleaners"
-            className={({ isActive }) =>
-              isActive ? "text-blue-600 font-bold" : "hover:text-blue-600"
-            }
-          >
-            <div className="flex flex-row items-center gap-3 p-2 ml-5 hover:bg-blue-600 hover:text-white">
-          <img src={cleanerIcon} alt="Dashboard icon" />
+          </div>
+        </NavLink>
+
+        <NavLink
+          to="/cleaners"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600 font-bold" : "hover:text-blue-600"
+          }
+        >
+          <div className="flex flex-row items-center gap-3 p-2 ml-5 hover:bg-blue-600 hover:text-white">
+            <img src={cleanerIcon} alt="Dashboard icon" />
             Cleaners
-            </div>
-          </NavLink>
-        
+          </div>
+        </NavLink>
       </div>
     </>
   );
--- client/PERN-Dashboard/src/pages/Cleaners.jsx
+++ client/PERN-Dashboard/src/pages/Cleaners.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-
+import cleanerIcon from "../assets/DefaultCleanerProfileImg.png";
+import emailIcon from "../assets/EmailIcon.svg";
+import phoneIcon from "../assets/PhoneIcon.svg";
 
 export default function Cleaners() {
   const [cleaners, setCleaners] = useState([]);
@@ -25,15 +27,31 @@ export default function Cleaners() {
   if (loading) return <div>Loading customers...</div>;
 
   return (
-    <div className="flex gap-10">
+    <div className="flex gap-10 flex-wrap justify-between">
       {cleaners.map((cleaner) => (
-        <div className="flex-row shadow-md">
-          <div>{cleaner.full_name}</div>
-          <div>{cleaner.email}</div>
-          <div>{cleaner.phone}</div>
+        // Cleaner Card
+        <div className="flex-row w-full sm:w-full md:w-full lg:w-1/4 p-4 shadow-md">
+          <div>
+            <img src={cleanerIcon} alt="Cleaner Profile Image" />
+          </div>
+          <div className="font-bold flex items-center justify-center">
+            {cleaner.full_name}
+          </div>
+          <div className="flex gap-2 items-center">
+            <img src={emailIcon} alt="" />
+            <span
+              class="w-full sm:w-auto truncate block max-w-full overflow-hidden text-ellipsis whitespace-nowrap"
+              title={cleaner.email}
+            >
+              {cleaner.email}
+            </span>
+          </div>
+          <div className="flex gap-2 items-center">
+            <img src={phoneIcon} alt="" />
+            {cleaner.phone}
+          </div>
         </div>
       ))}
     </div>
-  )
-
+  );
 }
