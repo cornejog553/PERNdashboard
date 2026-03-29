6916e6888f00165f920dc2974cb7167cfdcc9cde
Giovanni Cornejo <cornejog553@gmail.com>
2026-03-28 21:43:16 -0700

    added post, put and delete routes for customers

--- server/routes/customers.js
+++ server/routes/customers.js
@@ -12,4 +12,64 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Create new Customer - require admin role
+router.post("/", auth, adminOnly, async (req, res) => {
+  try {
+    const { full_name, email, phone, address } = req.body;
+    
+    const result = await pool.query(
+      `INSERT INTO customers (full_name, email, phone, address) 
+       VALUES ($1, $2, $3, $4) 
+       RETURNING *`,
+      [full_name, email, phone, address]
+    );
+    
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+
+router.put("/:id", auth, adminOnly, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { full_name, email, phone, address } = req.body;
+    
+    const result = await pool.query(
+      `UPDATE customers 
+       SET full_name = $1, email = $2, phone = $3, address = $4 
+       WHERE id = $5 
+       RETURNING *`,
+      [full_name, email, phone, address, id]
+    );
+    
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+
+// Delete Customer by id - require admin role
+router.delete("/:id", auth, adminOnly, async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await pool.query(
+      `DELETE FROM customers WHERE id = $1 RETURNING *`,
+      [id]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "Customer not found" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
